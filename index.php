07d535f60252e2b4cfd738d1f60e7bfbecaceda6
parvezmahmud-developer <parvezmahmud78@gmail.com>
2020-11-08 13:11:51 +0600

    data type

--- index.php
+++ index.php
@@ -1,2 +1,29 @@
-$name='parvez';
-echo $name;
+<?php
+$radius=25;
+$pi=3.14;
+//echo $pi**2;
+//order of calculation(BIDMAS);
+//echo $radius++;
+//echo $radius;
+//$age=30;
+//$age=$age+10;
+//$age-=10;
+//$age*=2;
+//echo $age;
+//echo floor($pi);
+//echo ceil($pi);
+echo pi();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1></h1>
+    
+</body>
+</html>
