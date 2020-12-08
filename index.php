e15b872f71d7f5bf68dc6a6fc945e497278e1382
parvezmahmud-developer <parvezmahmud78@gmail.com>
2020-12-08 10:21:54 +0600

    rendering data to the browser

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ if(!$conn){
     echo 'connection error: ' . mysqli_connect_error();
 }
 //write query for all pizzas
-$sql = 'SELECT title, ingredients, id FROM pizzas';
+$sql = 'SELECT title, ingredients, id FROM pizzas ORDER 
+BY created_at';
 //make the query and get result
 $result = mysqli_query($conn, $sql);
 //fetch the resulting rows as an arrays
@@ -16,7 +17,7 @@ mysqli_free_result($result);
 //closing the connection
 mysqli_close($conn);
 
-print_r($pizzas);
+
 
 
 ?>
@@ -25,6 +26,40 @@ print_r($pizzas);
 <?php
 include('templates/header.php');
 ?>
+<h4 class="center grey-text">Pizzas!</h4>
+<div class="container">
+    <div class="row">
+        <?php foreach($pizzas as $pizza){ ?>
+            <div class="col s6 md3">
+                <div class="card z-depth-0">
+
+                    <div class="card-content center">
+                        <h6>
+                            <?php echo htmlspecialchars($pizza['title']); ?>
+                        
+                        </h6>
+                        <div>
+                            <?php echo htmlspecialchars($pizza['ingredients']); ?>
+                        
+                        
+                        </div>
+                    </div>
+                    <div class="card-action right-align">
+
+                        <a href="#" class="brand-text">More info!</a>
+                    
+                    </div>
+                
+                </div>
+            
+            </div>
+
+        <?php } ?>
+    
+    </div>
+
+</div>
+
 <?php include('templates/footer.php');?>
 
 
